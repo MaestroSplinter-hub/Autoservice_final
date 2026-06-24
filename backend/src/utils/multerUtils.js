bd44f5e1206a51bcc541a96efa08e64b4181667c
Tomas Llop <tomasignaciollop@gmail.com>
2026-06-24 16:27:28 -0300

    Primera version admin implementada

--- backend/src/utils/multerUtils.js
+++ backend/src/utils/multerUtils.js
@@ -1,15 +1,23 @@
 import path from 'path';
 import { fileURLToPath } from "url";
 import multer from 'multer';
+import fs from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+export const crearDirectorio = (directorio) => fs.mkdirSync(directorio, { recursive: true });
+
+export const asegurarUploadsDir = () => {
+    const rutaDir = path.join(__dirname, '../public/uploads');
+    crearDirectorio(rutaDir);
+    return rutaDir;
+};
+
 // Donde se va a guardar el archivo
-const definirDestino = (req, archivo, cb) => {
-    const rutaCarpeta = path.join(__dirname, '../public/uploads');
-    cb(null, rutaCarpeta);
-}
+const definirDestino = async (req, archivo, cb) => {
+    cb(null, asegurarUploadsDir());
+};
 
 // Le armo un nombre unico al archivo
 const definirNombreArchivo = (req, archivo, cb) => {
