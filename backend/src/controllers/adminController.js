9d17c351917fd778d1af18abe599fda1b9a329d8
Tomas Llop <tomasignaciollop@gmail.com>
2026-06-24 19:37:42 -0300

    abstrayendo

--- backend/src/controllers/adminController.js
+++ backend/src/controllers/adminController.js
@@ -1,6 +1,12 @@
 import { Producto } from "../models/index.js";
 import { crearProducto, actualizarProducto, cambiarEstadoProducto } from "../services/productoServices.js";
 
+const volverADashboard = (res) => res.redirect('/admin/dashboard');
+
+const volverALogin = (res) => res.redirect('/login');
+
+const volverALoginErr = (res) => () => res.status(500).redirect('/login');
+
 export const mostrarDashboardAdmin = async (req, res) => {
     try {
         const productosVideojuego = await Producto.findAll({ where: { tipo: 'videojuego' }, order: [['id','ASC']] });
@@ -23,8 +29,8 @@ export const agregarProductoAdmin = (req, res) => {
     const datos = { nombre, precio, tipo, imagen };
 
     crearProducto(datos)
-        .then(() => res.redirect('/admin/dashboard'))
-        .catch(() => res.status(500).redirect('/login'));
+        .then(volverADashboard(res))
+        .catch(volverALoginErr(res));
 };
 
 export const editarProductoAdmin = (req, res) => {
@@ -35,21 +41,21 @@ export const editarProductoAdmin = (req, res) => {
     if (req.file) datos.imagen = `/uploads/${req.file.filename}`;
 
     actualizarProducto(id, datos)
-        .then(() => res.redirect('/admin/dashboard'))
-        .catch(() => res.status(500).redirect('/login'));
+        .then(volverADashboard(res))
+        .catch(volverALoginErr(res));
 };
 
 export const desactivarProductoAdmin = (req, res) => {
     const { id } = req.params;
     cambiarEstadoProducto(id, false)
-        .then(() => res.redirect('/admin/dashboard'))
-        .catch(() => res.redirect('/login'));
+        .then(volverADashboard(res))
+        .catch(volverALogin(res));
 };
 
 export const activarProductoAdmin = (req, res) => {
     const { id } = req.params;
     cambiarEstadoProducto(id, true)
-        .then(() => res.redirect('/admin/dashboard'))
-        .catch(() => res.redirect('/login'));
+        .then(volverADashboard(res))
+        .catch(volverALogin(res));
 };
 
